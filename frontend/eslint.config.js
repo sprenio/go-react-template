9c4b8aa381e9b5ae2ba3cb0f362de75eceb7e6f8
sprenio <piotr.skrouba@gmail.com>
2026-01-16 05:51:48 +0100

    update eslint/prettier configuration
    (cherry picked from commit c5e84f4be15768ed384a7b45e3acf1e57d68edb0)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,12 +3,13 @@ import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -22,7 +23,6 @@ export default tseslint.config(
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier,
     },
     settings: {
       react: {
@@ -30,9 +30,6 @@ export default tseslint.config(
       },
     },
     rules: {
-      /* Prettier */
-      'prettier/prettier': 'error',
-
       /* React */
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
